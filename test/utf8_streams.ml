f371f206afedfad872a1504bc5fecd69a3fc7e42
Armin Heller <support@arminheller.de>
2022-08-29 12:59:30 +0200

    fix missing renamings

--- test/utf8_streams.ml
+++ test/utf8_streams.ml
@@ -1,4 +1,4 @@
 let assertTrue condition message =
   assert (condition || (print_endline message ; condition)) ;;
 
-assertTrue (Utf8_streams_ocaml.times_two 2 = 4) "Two times two should be four!" ;;
+assertTrue (Utf8_streams.times_two 2 = 4) "Two times two should be four!" ;;
